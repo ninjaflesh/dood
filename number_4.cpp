2e1f2480a071bb22b0e69ea6331d7dec7949f90f
Philip Graftio <49476875+ninjaflesh@users.noreply.github.com>
2019-06-06 19:57:38 +0300

    Функция смены регистра гласных букв

---
+++ number_4.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include <cctype>
+#include <string>
+using namespace std;
+
+void registr(string &s) {
+	for (int i = 0; i < s.length(); i++) {
+		if (s[i] == 'A' || s[i] == 'O' || s[i] == 'E' || s[i] == 'Y' || s[i] == 'U' || s[i] == 'I') {
+			s[i] = tolower(s[i]);
+		}
+	}
+}
+
+int main() {
+	string str;
+	getline(cin, str);
+	registr(str);
+	cout << str << endl;
+	return 0;
+}
